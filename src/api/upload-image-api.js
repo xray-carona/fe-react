b17ea313c8fb591f1c12757d4f7532a6e2e912a1
Amritesh Srivastava <amritesh.srivastava@booking.com>
2020-04-03 12:00:25 +0530

    add patientinfo to API request

--- src/api/upload-image-api.js
+++ src/api/upload-image-api.js
@@ -7,6 +7,6 @@ const config = {
 };
 
 const uploadImageToAwsS3 = async photo =>  await axios.post('/api/v1/upload', photo, config);
-const getMLResponse = async url =>  await axios.post('/api/v1/getMLResponse', url, config);
+const getMLResponse = async (url, patientInfo) =>  await axios.post('/api/v1/getMLResponse', {url:url, patientInfo:patientInfo}, config);
 
 export default { uploadImageToAwsS3, getMLResponse}
