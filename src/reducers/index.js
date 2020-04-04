07327860c84f523f37f7960534786a090edc18e7
Amritesh Srivastava <amritesh.srivastava@booking.com>
2020-04-04 15:53:18 +0530

    removed state from App component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,24 +1,33 @@
 import { actionTypes } from '../actions'
 
 export const InitialState = {
+  xray_image: '',
   msg: '',
   type: '',
-  aws_s3_image_url: ''
+  aws_s3_image_url: '',
+  loading: false
 }
 
 function reducer (state = InitialState, action) {
   switch (action.type) {
     case actionTypes.SET_AWS_S3_IMAGE_URL:
-      state.loading = false;
       return {
         ...state,
-        ...{ aws_s3_image_url: action.url }
+        ...{ aws_s3_image_url: action.url,
+            loading: false }
+      }
+
+    case actionTypes.SET_XRAY_IMAGE:
+      return {
+        ...state,
+        ...{ xray_image: action.xray_image
+            }
       }
 
     case actionTypes.RESPONSE:
       return {
         ...state,
-        ...{ msg: action.data._response, type: action.data._type  }
+        ...{ msg: action.data._response, type: action.data._type, loading: false  }
       }
 
     case actionTypes.SET_ML_RESPONSE:
@@ -29,6 +38,11 @@ function reducer (state = InitialState, action) {
               annotated_img_url: action.ml_response.annotated_img_url 
             }
     }
+    case actionTypes.SET_LOADING:
+      return {
+        ...state,
+        ...{ loading: action.loading  }
+      }
 
     default:
       return state
