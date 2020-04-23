17d8d4067040e4eb7d2dc62217490eeecaf927f0
Govinda <govi2010@gmail.com>
2020-04-23 14:50:17 +0530

    Commit --- half result page

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,6 +9,7 @@ import {clearResults, logout} from "../actions";
 import PropTypes from 'prop-types';
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
+
 class Results extends Component {
 
     backToPatientInfo = e => {
@@ -58,7 +59,8 @@ class Results extends Component {
                     {(this.props.covid_diagnosis && !this.props.loading) ?
                         <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <h5>Test #420</h5>
-                            <p>System Generated Results not to be consumed without the supervision of Registered Physician</p>
+                            <p>System Generated Results not to be consumed without the supervision of Registered
+                                Physician</p>
                         </div> : null}
                     <div className="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 d-flex flex-column">
                         <h5 className="w-100 d-block text-center mt-3 mb-3">Input Image</h5>
@@ -82,116 +84,120 @@ class Results extends Component {
                         <LungConditions data={this.props.lung_conditions}/>
                     </div>
                 </div>
-                {/*<div className="row mt-4">*/}
-                {/*    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12 d-flex flex-column">*/}
-                {/*        <strong className="mt-2">Patient #69</strong>*/}
-                {/*        <strong className="mt-2">Bhupendra Yadav</strong>*/}
-                {/*        <strong className="mt-2">35 M</strong>*/}
-                {/*        <strong className="mt-2">37 deg Celsius</strong>*/}
-                {/*        <strong className="mt-2">Sneezing <span className="text-blue"> NO </span> </strong>*/}
-                {/*        <strong className="mt-2">Breathing issues <span className="text-pink">Yes</span></strong>*/}
-                {/*        <strong className="mt-2">Dry Cough <span className="text-pink">Yes</span></strong>*/}
-                {/*    </div>*/}
-                {/*    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">*/}
-                {/*        <strong>Diagnosis</strong>*/}
-                {/*        <h1 className="mt-5">COVID-19</h1>*/}
-                {/*    </div>*/}
-                {/*    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">*/}
-                {/*        <label className="radio-btn ml-3 text-pink mt-5" style={{fontSize: '34px',fontWeight: '700'}}>POSITIVE*/}
-                {/*            <input type="radio" name="radio">*/}
-                {/*                <span className="checkmark" style={{marginTop: '14px'}}></span>*/}
-                {/*            </input>*/}
-                {/*        </label>*/}
-                {/*        <label className="radio-btn ml-3 text-blue" style={{fontSize: '34px',fontWeight: '700'}}>NEGATIVE*/}
-                {/*            <input type="radio" name="radio">*/}
-                {/*                <span className="checkmark" style={{marginTop: '14px'}}></span>*/}
-                {/*            </input>*/}
-                {/*        </label>*/}
-                {/*    </div>*/}
-                {/*    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12 d-flex flex-column">*/}
-                {/*        <button className="btn btn-secondary mt-3">View Report</button>*/}
-                {/*        <button className="btn btn-secondary mt-3">Print Report</button>*/}
-                {/*        <button className="btn btn-secondary mt-3">Share Report</button>*/}
+                <div className="row mt-4">
+                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12 d-flex flex-column">
+                        <strong className="mt-2">Patient #69</strong>
+                        <strong className="mt-2">Bhupendra Yadav</strong>
+                        <strong className="mt-2">35 M</strong>
+                        <strong className="mt-2">37 deg Celsius</strong>
+                        <strong className="mt-2">Sneezing <span className="text-blue"> NO </span> </strong>
+                        <strong className="mt-2">Breathing issues <span className="text-pink">Yes</span></strong>
+                        <strong className="mt-2">Dry Cough <span className="text-pink">Yes</span></strong>
+                    </div>
+                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                        <strong>Diagnosis</strong>
+                        <h1 className="mt-5">COVID-19</h1>
+                    </div>
+                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                        <label className="radio-btn ml-3 text-pink mt-5" style={{fontSize: '34px',fontWeight: '700'}}>POSITIVE
+                            <input type="radio" name="radio"/>
+                                <span className="checkmark" style={{marginTop: '14px'}}></span>
+                        </label>
+                        <label className="radio-btn ml-3 text-blue" style={{fontSize: '34px',fontWeight: '700'}}>NEGATIVE
+                            <input type="radio" name="radio"/>
+                                <span className="checkmark" style={{marginTop: '14px'}}></span>
+                        </label>
+                    </div>
+                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12 d-flex flex-column">
+                        <button className="btn btn-secondary mt-3">View Report</button>
+                        <button className="btn btn-secondary mt-3">Print Report</button>
+                        <button className="btn btn-secondary mt-3">Share Report</button>
 
-                {/*    </div>*/}
-                {/*</div>*/}
-                {/*<div className="row justify-content-center">*/}
-                {/*    <div className="col-xl-7 col-lg-7 col-md-7 col-xs-12 col-sm-12">*/}
-                {/*        <div className="input-label-up color-p">*/}
-                {/*            <label>Investigation Summary</label>*/}
-                {/*            <textarea className="form-control" placeholder="Type here to add relevant details"*/}
-                {/*                      type="text"></textarea>*/}
-                {/*        </div>*/}
-                {/*        <div className="input-label-up color-p mt-5">*/}
-                {/*            <label>Action Taken</label>*/}
-                {/*            <textarea className="form-control" placeholder="Type here to add relevant details"*/}
-                {/*                      type="text"></textarea>*/}
-                {/*        </div>*/}
-                {/*        <div className="d-flex mt-3">*/}
-                {/*            <strong>Sent for PCR</strong>*/}
-                {/*            <label className="radio-btn ml-3">yes*/}
-                {/*                <input type="radio" name="radio">*/}
-                {/*                    <span className="checkmark"></span>*/}
-                {/*                </input>*/}
-                {/*            </label>*/}
-                {/*            <label className="radio-btn ml-3">No*/}
-                {/*                <input type="radio" name="radio">*/}
-                {/*                    <span className="checkmark"></span>*/}
-                {/*                </input>*/}
-                {/*            </label>*/}
-                {/*        </div>*/}
-                {/*        <div className="d-flex mt-3">*/}
-                {/*            <strong>Quarantime</strong>*/}
-                {/*            <label className="radio-btn ml-3">yes*/}
-                {/*                <input type="radio" name="radio">*/}
-                {/*                    <span className="checkmark"></span>*/}
-                {/*                </input>*/}
-                {/*            </label>*/}
-                {/*            <label className="radio-btn ml-3">No*/}
-                {/*                <input type="radio" name="radio">*/}
-                {/*                    <span className="checkmark"></span>*/}
-                {/*                </input>*/}
-                {/*            </label>*/}
-                {/*        </div>*/}
-                {/*        <div className="d-flex mt-3">*/}
-                {/*            <strong>Contact Tracing</strong>*/}
-                {/*            <label className="radio-btn ml-3">yes*/}
-                {/*                <input type="radio" name="radio">*/}
-                {/*                    <span className="checkmark"></span>*/}
-                {/*                </input>*/}
-                {/*            </label>*/}
-                {/*            <label className="radio-btn ml-3">No*/}
-                {/*                <input type="radio" name="radio">*/}
-                {/*                    <span className="checkmark"></span>*/}
-                {/*                </input>*/}
-                {/*            </label>*/}
-                {/*        </div>*/}
-                {/*        <div className="mt-5 text-center">*/}
-                {/*            <button className="btn btn-secondary" style={{paddingLeft: '50px',paddingRight: '50px'}}>Next*/}
-                {/*                Patient*/}
-                {/*            </button>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-xl-7 col-lg-7 col-md-7 col-xs-12 col-sm-12">
+                        <div className="input-label-up color-p">
+                            <label>Investigation Summary</label>
+                            <textarea className="form-control" placeholder="Type here to add relevant details"
+                                      type="text"></textarea>
+                        </div>
+                        <div className="input-label-up color-p mt-5">
+                            <label>Action Taken</label>
+                            <textarea className="form-control" placeholder="Type here to add relevant details"
+                                      type="text"></textarea>
+                        </div>
+                        <div className="d-flex mt-3">
+                            <strong>Sent for PCR</strong>
+                            <label className="radio-btn ml-3">yes
+                                <input type="radio" name="radio"/>
+                                    <span className="checkmark"></span>
+                            </label>
+                            <label className="radio-btn ml-3">No
+                                <input type="radio" name="radio"/>
+                                    <span className="checkmark"></span>
+                            </label>
+                        </div>
+                        <div className="d-flex mt-3">
+                            <strong>Quarantime</strong>
+                            <label className="radio-btn ml-3">yes
+                                <input type="radio" name="radio"/>
+                                    <span className="checkmark"></span>
+                            </label>
+                            <label className="radio-btn ml-3">No
+                                <input type="radio" name="radio"/>
+                                    <span className="checkmark"></span>
+                            </label>
+                        </div>
+                        <div className="d-flex mt-3">
+                            <strong>Contact Tracing</strong>
+                            <label className="radio-btn ml-3">yes
+                                <input type="radio" name="radio"/>
+                                    <span className="checkmark"></span>
+                            </label>
+                            <label className="radio-btn ml-3">No
+                                <input type="radio" name="radio"/>
+                                    <span className="checkmark"></span>
+                            </label>
+                        </div>
+                        <div className="mt-5 text-center">
+                            <button className="btn btn-secondary"
+                                    style={{paddingLeft: '50px', paddingRight: '50px'}}>Next
+                                Patient
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-    );
+        );
     }
-    }
+}
 
-    Results.propTypes = {
-        xray_image: PropTypes.object,
-        ct_scan_image: PropTypes.object,
-        lung_conditions: PropTypes.object,
-        aws_s3_image_url: PropTypes.string,
-        msg: PropTypes.string,
-        type: PropTypes.string,
-        covid_diagnosis: PropTypes.string,
-        annotated_img_url: PropTypes.string,
-        loading: PropTypes.bool
-    }
+Results.propTypes = {
+    xray_image: PropTypes.object,
+    ct_scan_image: PropTypes.object,
+    lung_conditions: PropTypes.object,
+    aws_s3_image_url: PropTypes.string,
+    msg: PropTypes.string,
+    type: PropTypes.string,
+    covid_diagnosis: PropTypes.string,
+    annotated_img_url: PropTypes.string,
+    loading: PropTypes.bool
+}
 
-    const mapStateToProps = ({xray_image, aws_s3_image_url,msg,type, covid_diagnosis, annotated_img_url, loading, lung_conditions, ct_scan_image, model_type}) => ({xray_image, aws_s3_image_url,msg,type, covid_diagnosis, annotated_img_url, loading, lung_conditions, ct_scan_image, model_type});
-    const mapDispatchToProps = dispatch => {
-        return bindActionCreators({clearResults, logout}, dispatch);
-    };
-    export default connect(mapStateToProps, mapDispatchToProps)(Results)
+const mapStateToProps = ({xray_image, aws_s3_image_url, msg, type, covid_diagnosis, annotated_img_url, loading, lung_conditions, ct_scan_image, model_type}) => ({
+    xray_image,
+    aws_s3_image_url,
+    msg,
+    type,
+    covid_diagnosis,
+    annotated_img_url,
+    loading,
+    lung_conditions,
+    ct_scan_image,
+    model_type
+});
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({clearResults, logout}, dispatch);
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Results)
