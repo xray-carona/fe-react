4c21a129b1f6c7d04e71bf2149b48871fb299c7c
Govinda <govi2010@gmail.com>
2020-04-23 10:05:53 +0530

    Commit --- half result page

--- src/components/LungConditions.js
+++ src/components/LungConditions.js
@@ -8,19 +8,51 @@ class LungConditions extends Component {
     if (this.props.data) {
       Object.keys(this.props.data).forEach(key => {
         tifOptions.push(
-          <div class="row">
-            <div class="col-md-6">{key}</div>
-            <div class="col-md-6">
-              <div>{this.props.data[key].result_boolean? <div><b>Yes</b></div> : <div>No</div>}</div>
+        <div className="row mt-3">
+          <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <strong>{key}</strong>
+          </div>
+
+          <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12">
+            <div className="slidecontainer">
+              <input type="range" min={this.props.data[key].low_val} max={this.props.data[key].high_val} value={this.props.data[key].this_val} className="slider" id={key}/>
             </div>
           </div>
+        </div>
         );
       });
     }
     return (
-      <div>
-        {tifOptions}
-      </div>
+        <React.Fragment>
+          <div className="mt-3 mb-3 text-center">
+            <h5>Disease Predictions</h5>
+          </div>
+          <div className="row mt-3">
+            <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+
+            </div>
+
+            <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12 ">
+              <div className="justify-content-between d-flex">
+                <small>Healthy</small>
+                <small>Risk</small>
+              </div>
+              <div className="justify-content-between d-flex dote">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
+          {tifOptions}
+        </React.Fragment>
       );
   }
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,116 +1,197 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { clearResults, logout} from '../actions';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import Preview from './Preview';
-import LungConditions from './LungConditions';
 import '../styles/App.css';
-import { setCookie } from '../util/cookies';
-
+import {setCookie} from '../util/cookies';
+import downloadIcon from '../assets/img/downo.png';
+import LungConditions from "./LungConditions";
+import {clearResults, logout} from "../actions";
+import PropTypes from 'prop-types';
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
 class Results extends Component {
 
     backToPatientInfo = e => {
-      e.preventDefault();
-      this.props.clearResults();
-      this.props.history.push('patientInfoForm');
+        e.preventDefault();
+        this.props.clearResults();
+        this.props.history.push('patientInfoForm');
     }
 
-  logout = e => {
-    e.preventDefault();
-    this.props.logout();
-    setCookie('token', '');
-    this.props.history.push('login');
-  }
+    logout = e => {
+        e.preventDefault();
+        this.props.logout();
+        setCookie('token', '');
+        this.props.history.push('login');
+    }
 
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/patientInfoForm"}><div className="text-white">New Patient</div></Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to='/patientInfoForm'><div className="text-white">Home</div></Link>
-                </li>
-                <li className="nav-item">
-                  <button className="btn-secondary" onClick= { e => this.logout(e)} style={{marginTop:'10%'}}><div className="text-white">Logout</div></button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      <div className="row">
-        <button className="upload-btn btn-primary text-white back-button" onClick={this.backToPatientInfo}>Back</button>
-      </div>
-      <div className="row container results">
-        <div class="col-md-12">
-        { (this.props.covid_diagnosis && !this.props.loading)?
-          <div className="col-lg-12 col-md-12 ">
-              <div className={`alert ${this.props.type} alert-dismissible mt-3`}>
-                <h4>{this.props.covid_diagnosis}</h4>
-            </div>
-          </div> : null }
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                    <div className="container">
+                        <Link className="navbar-brand" to={"/patientInfoForm"}>
+                            <div className="text-white">New Patient</div>
+                        </Link>
+                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to='/patientInfoForm'>
+                                        <div className="text-white">Home</div>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="btn-secondary" onClick={e => this.logout(e)}
+                                            style={{marginTop: '10%'}}>
+                                        <div className="text-white">Logout</div>
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="row">
+                    <button className="upload-btn btn-primary text-white back-button"
+                            onClick={this.backToPatientInfo}>Back
+                    </button>
+                </div>
+                <div className="row">
+                    {(this.props.covid_diagnosis && !this.props.loading) ?
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <h5>Test #420</h5>
+                            <p>System Generated Results not to be consumed without the supervision of Registered Physician</p>
+                        </div> : null}
+                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 d-flex flex-column">
+                        <h5 className="w-100 d-block text-center mt-3 mb-3">Input Image</h5>
+                        {this.props.model_type === 'xray' ? <Preview file={this.props.xray_image}/> :
+                            this.props.model_type === 'ct' ? <Preview file={this.props.ct_scan_image}/> : null}
+                    </div>
+                    {this.props.annotated_img_url !== '' ?
+                        <div className="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-6 d-flex flex-column">
+                            <h5 className="w-100 d-block text-center mt-3 mb-3">Annotated Image</h5>
+                            <div>
+                                <img alt="annotated" className="w-100" src={this.props.annotated_img_url}/>
+                            </div>
+                            <div className="text-center">
+                                <button className="btn btn-link">
+                                    <img alt="download" style={{height: '30px'}} src={downloadIcon}/>
+                                </button>
+                            </div>
+                        </div>
+                        : null}
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <LungConditions data={this.props.lung_conditions}/>
+                    </div>
+                </div>
+                {/*<div className="row mt-4">*/}
+                {/*    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12 d-flex flex-column">*/}
+                {/*        <strong className="mt-2">Patient #69</strong>*/}
+                {/*        <strong className="mt-2">Bhupendra Yadav</strong>*/}
+                {/*        <strong className="mt-2">35 M</strong>*/}
+                {/*        <strong className="mt-2">37 deg Celsius</strong>*/}
+                {/*        <strong className="mt-2">Sneezing <span className="text-blue"> NO </span> </strong>*/}
+                {/*        <strong className="mt-2">Breathing issues <span className="text-pink">Yes</span></strong>*/}
+                {/*        <strong className="mt-2">Dry Cough <span className="text-pink">Yes</span></strong>*/}
+                {/*    </div>*/}
+                {/*    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">*/}
+                {/*        <strong>Diagnosis</strong>*/}
+                {/*        <h1 className="mt-5">COVID-19</h1>*/}
+                {/*    </div>*/}
+                {/*    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">*/}
+                {/*        <label className="radio-btn ml-3 text-pink mt-5" style={{fontSize: '34px',fontWeight: '700'}}>POSITIVE*/}
+                {/*            <input type="radio" name="radio">*/}
+                {/*                <span className="checkmark" style={{marginTop: '14px'}}></span>*/}
+                {/*            </input>*/}
+                {/*        </label>*/}
+                {/*        <label className="radio-btn ml-3 text-blue" style={{fontSize: '34px',fontWeight: '700'}}>NEGATIVE*/}
+                {/*            <input type="radio" name="radio">*/}
+                {/*                <span className="checkmark" style={{marginTop: '14px'}}></span>*/}
+                {/*            </input>*/}
+                {/*        </label>*/}
+                {/*    </div>*/}
+                {/*    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12 d-flex flex-column">*/}
+                {/*        <button className="btn btn-secondary mt-3">View Report</button>*/}
+                {/*        <button className="btn btn-secondary mt-3">Print Report</button>*/}
+                {/*        <button className="btn btn-secondary mt-3">Share Report</button>*/}
 
-      { this.props.model_type === 'xray' ?
-        <div className="row">
-          <div className="col-md-4">
-            <h2>Input XRay image</h2>
-            <Preview file={this.props.xray_image} />
-          </div>
-          { this.props.annotated_img_url ?
-              <div className="col-md-4">
-                <img src={this.props.annotated_img_url} alt={'annotated_img_url'} className="img-thumbnail" />
+                {/*    </div>*/}
+                {/*</div>*/}
+                {/*<div className="row justify-content-center">*/}
+                {/*    <div className="col-xl-7 col-lg-7 col-md-7 col-xs-12 col-sm-12">*/}
+                {/*        <div className="input-label-up color-p">*/}
+                {/*            <label>Investigation Summary</label>*/}
+                {/*            <textarea className="form-control" placeholder="Type here to add relevant details"*/}
+                {/*                      type="text"></textarea>*/}
+                {/*        </div>*/}
+                {/*        <div className="input-label-up color-p mt-5">*/}
+                {/*            <label>Action Taken</label>*/}
+                {/*            <textarea className="form-control" placeholder="Type here to add relevant details"*/}
+                {/*                      type="text"></textarea>*/}
+                {/*        </div>*/}
+                {/*        <div className="d-flex mt-3">*/}
+                {/*            <strong>Sent for PCR</strong>*/}
+                {/*            <label className="radio-btn ml-3">yes*/}
+                {/*                <input type="radio" name="radio">*/}
+                {/*                    <span className="checkmark"></span>*/}
+                {/*                </input>*/}
+                {/*            </label>*/}
+                {/*            <label className="radio-btn ml-3">No*/}
+                {/*                <input type="radio" name="radio">*/}
+                {/*                    <span className="checkmark"></span>*/}
+                {/*                </input>*/}
+                {/*            </label>*/}
+                {/*        </div>*/}
+                {/*        <div className="d-flex mt-3">*/}
+                {/*            <strong>Quarantime</strong>*/}
+                {/*            <label className="radio-btn ml-3">yes*/}
+                {/*                <input type="radio" name="radio">*/}
+                {/*                    <span className="checkmark"></span>*/}
+                {/*                </input>*/}
+                {/*            </label>*/}
+                {/*            <label className="radio-btn ml-3">No*/}
+                {/*                <input type="radio" name="radio">*/}
+                {/*                    <span className="checkmark"></span>*/}
+                {/*                </input>*/}
+                {/*            </label>*/}
+                {/*        </div>*/}
+                {/*        <div className="d-flex mt-3">*/}
+                {/*            <strong>Contact Tracing</strong>*/}
+                {/*            <label className="radio-btn ml-3">yes*/}
+                {/*                <input type="radio" name="radio">*/}
+                {/*                    <span className="checkmark"></span>*/}
+                {/*                </input>*/}
+                {/*            </label>*/}
+                {/*            <label className="radio-btn ml-3">No*/}
+                {/*                <input type="radio" name="radio">*/}
+                {/*                    <span className="checkmark"></span>*/}
+                {/*                </input>*/}
+                {/*            </label>*/}
+                {/*        </div>*/}
+                {/*        <div className="mt-5 text-center">*/}
+                {/*            <button className="btn btn-secondary" style={{paddingLeft: '50px',paddingRight: '50px'}}>Next*/}
+                {/*                Patient*/}
+                {/*            </button>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
-            : null }
-          { this.props.lung_conditions ?
-              <div className="col-md-4">
-                <h2>Disease predictions</h2>
-                <LungConditions data={this.props.lung_conditions} />
-            </div>
-            : null }
-        </div> : null }
-
-        { this.props.model_type === 'ct' ?
-        <div className="row">
-          <div className="col-md-4">
-            <h2>Input CT scan</h2>
-            <Preview file={this.props.ct_scan_image} />
-          </div>
-          { this.props.annotated_img_url ?
-              <div className="col-md-4">
-                <h2>Annotated CT scan</h2>
-                <img src={this.props.annotated_img_url} alt={'annotated_img_url'} className="img-thumbnail" />
-            </div>
-            : null }
-          { this.props.lung_conditions ?
-              <div className="col-md-4">
-                <h2>Disease predictions</h2>
-                <LungConditions data={this.props.lung_conditions} />
-            </div>
-            : null }
-        </div> : null }
-
-        </div>
-      </div>
-      </div>
     );
-  }
-}
-Results.propTypes = {
-    xray_image: PropTypes.object,
-    ct_scan_image: PropTypes.object,
-    lung_conditions: PropTypes.object,
-    aws_s3_image_url: PropTypes.string,
-    msg: PropTypes.string,
-    type: PropTypes.string,
-    covid_diagnosis : PropTypes.string,
-    annotated_img_url: PropTypes.string,
-    loading: PropTypes.bool
-}
+    }
+    }
 
-const mapStateToProps = ({xray_image, aws_s3_image_url,msg,type, covid_diagnosis, annotated_img_url, loading, lung_conditions, ct_scan_image, model_type}) => ({xray_image, aws_s3_image_url,msg,type, covid_diagnosis, annotated_img_url, loading, lung_conditions, ct_scan_image, model_type});
-const mapDispatchToProps = dispatch => bindActionCreators( {clearResults, logout}, dispatch);
-export default connect(mapStateToProps, mapDispatchToProps)(Results)
+    Results.propTypes = {
+        xray_image: PropTypes.object,
+        ct_scan_image: PropTypes.object,
+        lung_conditions: PropTypes.object,
+        aws_s3_image_url: PropTypes.string,
+        msg: PropTypes.string,
+        type: PropTypes.string,
+        covid_diagnosis: PropTypes.string,
+        annotated_img_url: PropTypes.string,
+        loading: PropTypes.bool
+    }
+
+    const mapStateToProps = ({xray_image, aws_s3_image_url,msg,type, covid_diagnosis, annotated_img_url, loading, lung_conditions, ct_scan_image, model_type}) => ({xray_image, aws_s3_image_url,msg,type, covid_diagnosis, annotated_img_url, loading, lung_conditions, ct_scan_image, model_type});
+    const mapDispatchToProps = dispatch => {
+        return bindActionCreators({clearResults, logout}, dispatch);
+    };
+    export default connect(mapStateToProps, mapDispatchToProps)(Results)
